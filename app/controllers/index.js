7b84be7fe39eaeefba2fe34a35a07c24e64af424
Regina Lee <reginalee@Reginas-MacBook-Pro.local>
2017-03-25 09:51:41 -0700

    feexet the controllers

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,38 +3,38 @@
 const db = require('../models/index.js');
 
 module.exports = {
-  // users: {
-  //   get: (req, res) => {
-  //     Users.find({}, (err, result) => {
-  //       if (err) {
-  //         throw err;
-  //         res.status(404);
-  //       } else {
-  //         console.log('successful user get');
-  //         res.status(200).send(result);
-  //       }
-  //     })
-  //     // res.end();
-  //   },
-  //   getById: (req, res) => {
-  //     res.end();
-  //   },
-  //   post: (req, res) => {
-  //     let newUser = newUser(req.body);
-  //     newUser.save((err, data) => {
-  //       if (err) {
-  //         throw err;
-  //       } else {
-  //         console.log('successful user post');
-  //         res.status(201);
-  //         res.json({
-  //         })
-  //       }
-  //     })
-  //     // console.log(req.body);
-  //     res.end();
-  //   },
-  // },
+  users: {
+    get: (req, res) => {
+      Users.find({}, (err, result) => {
+        if (err) {
+          throw err;
+          res.status(404);
+        } else {
+          console.log('successful user get');
+          res.status(200).send(result);
+        }
+      })
+      // res.end();
+    },
+    getById: (req, res) => {
+      res.end();
+    },
+    post: (req, res) => {
+      let newUser = newUser(req.body);
+      newUser.save((err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log('successful user post');
+          res.status(201);
+          res.json({
+          })
+        }
+      })
+      // console.log(req.body);
+      res.end();
+    },
+  },
   memes: {
     // finds a random url from database. We have 50 pictures and the id numbering starts at 1
     get: (req, res) => {
@@ -45,7 +45,6 @@ module.exports = {
       .then((meme) => {
         console.log('THE MEME BROASDFASDFASDFASDFASDFSDFASDFSDF', meme)
         res.send(meme[0].dataValues.href);
-        res.end();
       })
       .catch((error) => {
         console.log('Error: ', error);
@@ -55,15 +54,15 @@ module.exports = {
       res.end();
     },
   },
-  // rooms: {
-  //   get: (req, res) => {
-  //     res.end();
-  //   },
-  //   getById: (req, res) => {
-  //     res.end();
-  //   },
-  //   post: (req, res) => {
-  //     res.end();
-  //   },
-  // },
+  rooms: {
+    get: (req, res) => {
+      res.end();
+    },
+    getById: (req, res) => {
+      res.end();
+    },
+    post: (req, res) => {
+      res.end();
+    },
+  },
 };
